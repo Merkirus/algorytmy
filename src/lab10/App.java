297360f3f8a350d154c2e4b1b52cda54127b7147
Rafał Mielniczuk <rmielniczuk001@gmail.com>
2022-05-22 20:07:44 +0200

    ;-;

--- src/lab10/App.java
+++ src/lab10/App.java
@@ -10,15 +10,10 @@ import java.util.HashMap;
 
 public class App {
     public static void main(String[] args) throws EmptyStackException, EmptyQueueException, FullQueueException, FullStackException {
-        HuffmanTree ht = new HuffmanTree();
-        HashMap<Character, Integer> map = ht.frequency();
-        ht.displayMap(map);
-        ht.generateTree(map);
-        ht.convertToCode();
-        ht.displayCode();
-        IntegerToStringExecutor executor = new IntegerToStringExecutor();
-        ht.inOrderWalk(executor);
-        System.out.println(executor.getResult());
-        ht.saveCodeToFile();
+        MorseTree mt = new MorseTree();
+        mt.translateMorse();
+        mt.generateTree();
+        mt.convertToCode();
+        mt.saveCodeToFile();
     }
 }
